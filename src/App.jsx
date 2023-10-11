9d55129645d0652fb0971c8ec7c22fdc4c4326a7
Alejitto <Alexcervantes2698@hotmail.com>
2023-10-11 17:16:29 -0600

    se creo barra de navegacion y estillos

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,50 @@ function App() {
         </div>
       </header>
       <main>
-
+        <div id='container-main'>
+          <div id='home' className='menu'>
+            <a href='#'>
+            <i className="bi bi-house-door-fill"></i>
+              <p>Inicio</p>
+            </a>
+          </div>
+          <div id='about-us' className='menu'>
+            <a href='#'>
+            <i className="bi bi-question-circle-fill"></i>
+              <p>¿Quiénes Somos?</p>
+            </a>
+          </div>
+          <div id='home' className='menu'>
+            <a href='#'>
+            <i className="bi bi-binoculars-fill"></i>
+              <p>Vision</p>
+            </a>
+          </div>
+          <div id='about-us' className='menu'>
+            <a href='#'>
+            <i className="bi bi-bullseye"></i>
+              <p>Mision</p>
+            </a>
+          </div>
+          <div id='about-us' className='menu'>
+            <a href='#'>
+            <i className="bi bi-gear-fill"></i>
+              <p>Trabajos</p>
+            </a>
+          </div>
+          <div id='home' className='menu'>
+            <a href='#'>
+            <i className="bi bi-cart-fill"></i>
+              <p>Servicios y Ventas</p>
+            </a>
+          </div>
+          <div id='about-us' className='menu'>
+            <a href='#'>
+            <i className="bi bi-person-fill"></i>
+              <p>Contacto</p>
+            </a>
+          </div>
+        </div>
       </main>
       <body>
 
